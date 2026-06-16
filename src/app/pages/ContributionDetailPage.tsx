f598103e8e138628bda6895bcc070d88c747103e
titohnf <titohnf@gmail.com>
2026-06-16 21:30:54 +0700

    fix: revert to gray-400

--- src/app/pages/ContributionDetailPage.tsx
+++ src/app/pages/ContributionDetailPage.tsx
@@ -888,7 +888,7 @@ function WorkflowStepsSidebar({ contribution: c }: { contribution: Contribution 
                           ? "font-semibold text-blue-700"
                           : isPast || isPastRejected
                             ? "font-medium text-gray-600"
-                            : "font-normal text-gray-500"
+                            : "font-normal text-gray-400"
                   }`}
                     onClick={() => setDetailState(detailState === state ? null : state)}
                   >
